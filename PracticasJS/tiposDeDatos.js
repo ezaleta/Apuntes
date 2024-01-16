216619921705836e46d3bb13c258e1a26f815646
Edgar <sladien9@gmail.com>
2024-01-15 18:56:26 -0600

    Archivo .js agregado y practica con comandos linux

---
+++ PracticasJS/tiposDeDatos.js
@@ -0,0 +1,40 @@
+//Tipos de datos
+/* 
+Datos primitivos
+-string            wstring char
+-number            int dint float real
+-undefined       
+-booleano
+-null
+-bigint()
+-symbol(nuevo tipo de datos)
+
+Datos compuestos
+-objetos
+-funciones
+*/
+
+/* Variables */
+let nombre = "Edgar";
+var apellido = "Gonzalez";
+let alert = "Hola Mundo";
+let nombreCompleto ="Edgar Gonzalez Zaleta";
+let edad = 29;
+let datos = ["Edgar", 29, true, undefined, null];
+let mascotas = ["perro", "gato", "pez",["perico", "caballo", "gallina"]];
+let persona = {
+    nombre: "Fulanito",
+    edad: 20,
+    amigos: ["Mengano", "Perengano", "Sutano"]
+}
+
+console.log("Hola mi nombre es " + nombreCompleto + " y mi edad es " + edad);
+console.log('Hola mi "nombre" es ' + nombreCompleto + ' y mi edad es ' + edad);
+//Template string
+console.log(`Hola mi "nombre" es ${nombreCompleto} y mi 'edad' es ${edad}`)
+/* alert(nombre); */
+console.log(datos[1]);
+//array multidimensional
+console.log(mascotas[3][1]);
+//Objeto
+console.log(persona.amigos[0]);
